477c5f4dcb1045af3074ce4e92720f3c3e544229
Kkun84 <kesuike84@gmail.com>
2020-09-23 00:08:52 +0900

    commit

--- test/test.py
+++ test/test.py
@@ -1,6 +1,13 @@
 import requests
 
 
-response = requests.post('http://127.0.0.1:5000/predict', files=dict(file=open('test/dog.png', 'rb')))
+response = requests.post(
+    "https://flask-pytorch-test.herokuapp.com/predict",
+    files=dict(image=open("test/dog.png", "rb")),
+)
+print(response.text)
 
+response = requests.post(
+    "http://127.0.0.1:5000/predict", files=dict(image=open("test/dog.png", "rb"))
+)
 print(response.text)
